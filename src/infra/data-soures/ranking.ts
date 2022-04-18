b1795cab3f30afe6ffc98e462fbac17e2c98aa6d
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-18 23:32:27 +0200

    feat: create fake api

---
+++ src/infra/data-soures/ranking.ts
@@ -0,0 +1,68 @@
+export const ranking = [
+  {
+    user: {
+      country: 'Moz',
+      name: 'Yazalde Filimone',
+    },
+    heroes: [
+      {
+        name: 'Free',
+        level: 30,
+      },
+      {
+        name: 'Connie',
+        level: 26,
+      },
+      {
+        name: 'Mabyn',
+        level: 27,
+      },
+    ],
+    data: 1726189090000,
+    score: 455,
+  },
+  {
+    user: {
+      country: 'Brazil',
+      name: 'Matheus Herique',
+    },
+    heroes: [
+      {
+        name: 'Azura',
+        level: 30,
+      },
+      {
+        name: 'Hogan',
+        level: 26,
+      },
+      {
+        name: 'caldeira',
+        level: 27,
+      },
+    ],
+    data: 1797732090000,
+    score: 455,
+  },
+  {
+    user: {
+      country: 'EUA',
+      name: 'Maicon',
+    },
+    heroes: [
+      {
+        name: 'Free',
+        level: 30,
+      },
+      {
+        name: 'Connie',
+        level: 26,
+      },
+      {
+        name: 'Mabyn',
+        level: 27,
+      },
+    ],
+    data: 17260162899000,
+    score: 455,
+  },
+];
