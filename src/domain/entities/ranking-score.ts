fa37d76bd50ce954209e073e73ec1da3b7a4987d
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-18 22:40:47 +0200

    feat: create ranking entities

---
+++ src/domain/entities/ranking-score.ts
@@ -0,0 +1,16 @@
+export type RankingStore = {
+  player: Player;
+  score: number;
+  matchdate: Date;
+  heros: Hero[];
+};
+
+type Player = {
+  name: string;
+  country: string;
+};
+
+type Hero = {
+  name: string;
+  lever: number;
+};
