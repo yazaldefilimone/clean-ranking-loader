06ff5f9c3afbca60b86cac213a4c47d80ff58315
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-19 11:22:48 +0200

    feat: create map colections

--- src/presentation/contracts/http.ts
+++ src/presentation/contracts/http.ts
@@ -2,3 +2,12 @@ export type HttpResponse<T = any> = {
   statusCode: number;
   data: T;
 };
+
+export const serverError = (error: any) => ({
+  statusCode: 500,
+  data: error.stack,
+});
+export const ok = (data: any) => ({
+  statusCode: 200,
+  data,
+});
